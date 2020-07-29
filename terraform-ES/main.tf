b83b06c3c0a540d5ad07e1dd8ac3fa929c5f7fec
sreenivasyanamala <33051135+sreenivasyanamala@users.noreply.github.com>
2020-07-29 13:07:34 +0530

    Update main.tf

--- terraform-ES/main.tf
+++ terraform-ES/main.tf
@@ -2,17 +2,17 @@ module "aws_es" {
 
   source = "./terraform-es"
 
-  domain_name           = "${var.domain_name}"
-  elasticsearch_version = "${var.elasticsearch_version}"
+  domain_name           = var.domain_name
+  elasticsearch_version = var.elasticsearch_version
 
   cluster_config_dedicated_master_enabled = true
-  cluster_config_instance_count           = "${var.instance_count}"
-  cluster_config_dedicated_master_type    =  "${var.master_instance_type}"
-  cluster_config_instance_type            = "${var.instance_type}"
+  cluster_config_instance_count           = var.instance_count
+  cluster_config_dedicated_master_type    =  var.master_instance_type
+  cluster_config_instance_type            = var.instance_type
   cluster_config_zone_awareness_enabled   = "true"
   cluster_config_availability_zone_count  = "2"
 
-  ebs_options_volume_size = "${var.volume_size}"
+  ebs_options_volume_size = var.volume_size
 
   encrypt_at_rest_enabled    = true
   encrypt_at_rest_kms_key_id = "alias/aws/es"
@@ -25,8 +25,8 @@ module "aws_es" {
   }
   
 vpc_options = {
-    subnet_ids = ["${var.subnetid_1}", "${var.subnetid_2}"]
-    security_group_ids = ["${var.security_group_id}"]
+    subnet_ids = [var.subnetid_1, var.subnetid_2]
+    security_group_ids = [var.security_group_id]
   }
 
   
