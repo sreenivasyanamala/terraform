e902f8548358e8ee7456fe255ecf9940d0b1659c
sreenivasyanamala <33051135+sreenivasyanamala@users.noreply.github.com>
2020-08-04 16:01:02 +0530

    Update main.tf

--- terraform-ES/terraform-es/main.tf
+++ terraform-ES/terraform-es/main.tf
@@ -15,8 +15,10 @@ resource "aws_elasticsearch_domain" "es_domain" {
   advanced_security_options {
     enabled = "true"
     internal_user_database_enabled = "true"
+    master_user_options {
     master_user_name = "test123"
     master_user_password = "Testing@123"
+      }
   }
   
   # ebs_options
